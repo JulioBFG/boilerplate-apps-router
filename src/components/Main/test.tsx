7a43f8853f120861cd584ff77c0583afac5f3f4e
Julio Cesar Lopes Filho <juliolopes.bfg@gmail.com>
2024-05-22 16:19:11 -0300

    changed stuff related to boilerplate

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,12 @@ describe('<Main />', () => {
       screen.getByRole('heading', { name: /react avancado/i })
     ).toBeInTheDocument()
   })
+
+  it('should render the colors correctly', () => {
+    // renderiza o component
+    const { container } = render(<Main />)
+
+    // verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
